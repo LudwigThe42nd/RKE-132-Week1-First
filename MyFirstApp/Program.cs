263d8a51ec81f83f7af2a2e5b78d136ddfe85270
PizzaBot <Breiton.Peerna@gmail.com>
2024-10-14 18:36:08 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,10 @@
+﻿// This is a Comment to my code
+
+
+Console.WriteLine("Enter your name:");
+
+string username = Console .ReadLine();
+
+Console.WriteLine("Hello" + ", " + username + "!");
+
+Console.WriteLine($"Hello { username}!");
